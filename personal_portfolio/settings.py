58ca28b711ae108817b0f9680356eceda0f57ff8
aphoo <ahphu@ucsd.edu>
2020-09-06 16:48:39 -0700

    Added heroku url to allowed_hosts in settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')4+fnixb9l_oy!jbguxv99^!5g&yl$y!z(tzr8*#^mom-)79ze'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aphu-portfolio.herokuapp.com']
 
 
 # Application definition
